650645345dc197ca5b7069085bf95185acc40467
Marius-Juston <Marius.juston@hotmail.fr>
2021-08-05 22:08:25 -0400

    removed ddpg unecessary functions

--- Python/Marius/ddpg/ddpg.py
+++ Python/Marius/ddpg/ddpg.py
@@ -3,109 +3,12 @@ import torch.autograd
 import torch.optim as optim
 from torch.autograd import Variable
 
+from ddpg.memory import Memory
 from ddpg.model import Critic
 
 
-def averaging(model, input):
-    # far
-    left = abs(model.layer['fuzzify'].varmfs[input].mfdefs['mf1'].a.item())
-    right = abs(model.layer['fuzzify'].varmfs[input].mfdefs['mf3'].d.item())
-    avg = (left + right) / 2
-    left = -avg
-    right = avg
-    with torch.no_grad():
-        model.layer['fuzzify'].varmfs[input].mfdefs['mf1'].a.copy_(torch.tensor(left, dtype=torch.float))
-        model.layer['fuzzify'].varmfs[input].mfdefs['mf3'].d.copy_(torch.tensor(right, dtype=torch.float))
-
-    # close far
-    left = abs(model.layer['fuzzify'].varmfs[input].mfdefs['mf1'].b.item())
-    right = abs(model.layer['fuzzify'].varmfs[input].mfdefs['mf3'].c.item())
-    avg = (left + right) / 2
-    left = -avg
-    right = avg
-    with torch.no_grad():
-        model.layer['fuzzify'].varmfs[input].mfdefs['mf1'].b.copy_(torch.tensor(left, dtype=torch.float))
-        model.layer['fuzzify'].varmfs[input].mfdefs['mf3'].c.copy_(torch.tensor(right, dtype=torch.float))
-
-    # near
-    left = abs(model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].a.item())
-    right = abs(model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].d.item())
-    avg = (left + right) / 2
-    left = -avg
-    right = avg
-    with torch.no_grad():
-        if input == 'distance_line':
-            if left > -0.125:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].a.copy_(torch.tensor(-0.125, dtype=torch.float))
-            else:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].a.copy_(torch.tensor(left, dtype=torch.float))
-            if right < 0.125:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].d.copy_(torch.tensor(0.125, dtype=torch.float))
-            else:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].d.copy_(torch.tensor(right, dtype=torch.float))
-        elif input == 'theta_near':
-            if left > -0.125:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].a.copy_(torch.tensor(-0.125, dtype=torch.float))
-            else:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].a.copy_(torch.tensor(left, dtype=torch.float))
-            if right < 0.125:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].d.copy_(torch.tensor(0.125, dtype=torch.float))
-            else:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].d.copy_(torch.tensor(right, dtype=torch.float))
-
-    # close_near
-    left = abs(model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].b.item())
-    right = abs(model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].c.item())
-    avg = (left + right) / 2
-    left = -avg
-    right = avg
-    with torch.no_grad():
-        if input == 'distance_line':
-            if left > -0.05:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].b.copy_(torch.tensor(-0.05, dtype=torch.float))
-            else:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].b.copy_(torch.tensor(left, dtype=torch.float))
-            if right < 0.05:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].c.copy_(torch.tensor(0.05, dtype=torch.float))
-            else:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].c.copy_(torch.tensor(right, dtype=torch.float))
-        elif input == 'theta_near':
-            if left > -0.025:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].b.copy_(torch.tensor(-0.025, dtype=torch.float))
-            else:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].b.copy_(torch.tensor(left, dtype=torch.float))
-            if right < 0.025:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].c.copy_(torch.tensor(0.025, dtype=torch.float))
-            else:
-                model.layer['fuzzify'].varmfs[input].mfdefs['mf2'].c.copy_(torch.tensor(right, dtype=torch.float))
-
-
-def mfs_constraint(model):
-    for i in range(len(model.input_keywords)):
-        for j in range(model.number_of_mfs[model.input_keywords[i]]):
-            averaging(model, model.input_keywords[i])
-
-            model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf0'].c = torch.tensor(
-                model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf1'].a.item())
-            model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf0'].d = torch.tensor(
-                model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf1'].b.item())
-            model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf1'].c = torch.tensor(
-                model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf2'].a.item())
-
-            model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf1'].d = torch.tensor(
-                model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf2'].b.item())
-            model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf3'].a = torch.tensor(
-                model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf2'].c.item())
-            model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf3'].b = torch.tensor(
-                model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf2'].d.item())
-            model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf4'].a = torch.tensor(
-                model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf3'].c.item())
-            model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf4'].b = torch.tensor(
-                model.layer['fuzzify'].varmfs[model.input_keywords[i]].mfdefs['mf3'].d.item())
-
-
 class DDPGagent:
-    def __init__(self, num_inputs, num_outputs, anf, hidden_size=128, actor_learning_rate=1e-3,
+    def __init__(self, num_inputs, num_outputs, anf, hidden_size=128, actor_learning_rate=1e-6 * 7,
                  critic_learning_rate=1e-4, gamma=0.99, tau=1e-3, max_memory_size=50000):
         # Params
         self.num_states = num_inputs
@@ -119,16 +22,10 @@ class DDPGagent:
         self.critic = Critic(self.num_states + self.num_actions, hidden_size, self.num_actions)
         self.critic_target = Critic(self.num_states + self.num_actions, hidden_size, self.num_actions)
 
-        for target_param, param in zip(self.actor_target.parameters(), self.actor.parameters()):
-            target_param.data.copy_(param.data)
-
-        for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
-            target_param.data.copy_(param.data)
-
         # Training
         self.memory = Memory(max_memory_size)
         self.critic_criterion = torch.nn.MSELoss(reduction='sum')
-        self.actor_optimizer = optim.SGD(self.actor.parameters(), lr=1e-6 * 7, momentum=0.99)
+        self.actor_optimizer = optim.SGD(self.actor.parameters(), lr=actor_learning_rate, momentum=0.99)
         self.critic_optimizer = optim.SGD(self.critic.parameters(), lr=critic_learning_rate, momentum=0.99)
 
     def get_action(self, state):
@@ -160,7 +57,7 @@ class DDPGagent:
         self.actor_optimizer.zero_grad()
         policy_loss.backward()
         self.actor_optimizer.step()
-        mfs_constraint(self.actor)
+
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
